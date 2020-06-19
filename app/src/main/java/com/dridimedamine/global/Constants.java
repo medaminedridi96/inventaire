4dd1964e4aa0af58dce684e05fb465d549f07242
Ahmed Radhouane Belkilani <arbelkilani@gmail.com>
2020-06-19 05:36:18 +0100

    - Adding Constants.java file inside global package in order to organise code and centralise all constants in one file class.

---
+++ app/src/main/java/com/dridimedamine/global/Constants.java
@@ -0,0 +1,9 @@
+package com.dridimedamine.global;
+
+public class Constants {
+
+    public static class Durations {
+        public static final long READ_TIMEOUT = 30000;
+        public static final long CONNECT_TIMEOUT = 30000;
+    }
+}
