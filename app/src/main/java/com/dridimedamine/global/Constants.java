f7bc829b81a441cfcbdd8041211b559d997c4544
Ahmed Radhouane Belkilani <arbelkilani@gmail.com>
2020-06-19 07:28:21 +0100

    - update Constants.java file by adding endpoint, HttpResponses and intent keys

--- app/src/main/java/com/dridimedamine/global/Constants.java
+++ app/src/main/java/com/dridimedamine/global/Constants.java
@@ -6,4 +6,19 @@ public class Constants {
         public static final long READ_TIMEOUT = 30000;
         public static final long CONNECT_TIMEOUT = 30000;
     }
+
+    public static class EndPoint {
+        public static final String LOGIN_USER = ""; //TODO add endpoint for login
+    }
+
+    public static class HttpResponses {
+
+        public static final int CODE_OK = 200;
+        public static final int NOT_FOUND = 404;
+    }
+
+    public static class IntentKeys {
+
+        public static final String USERNAME = "username";
+    }
 }
