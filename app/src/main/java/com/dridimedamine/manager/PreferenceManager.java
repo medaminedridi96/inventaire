964ee1279aca8136561cde30ddec6b32ce729bc7
Ahmed Radhouane Belkilani <arbelkilani@gmail.com>
2020-06-19 07:30:29 +0100

    - Adding shared preferences module by creating a singleton of PreferenceManager class.

---
+++ app/src/main/java/com/dridimedamine/manager/PreferenceManager.java
@@ -0,0 +1,37 @@
+package com.dridimedamine.manager;
+
+import android.content.SharedPreferences;
+
+import com.dridimedamine.InventoryApp;
+
+public class PreferenceManager {
+    private static final String FILE_NAME_FLAG = "preference_file_name";
+
+    private static PreferenceManager mInstance;
+    private SharedPreferences mSharedPreferences;
+
+    private PreferenceManager() {
+        mSharedPreferences = InventoryApp.getInstance().getSharedPreferences(FILE_NAME_FLAG, 0);
+    }
+
+    public static PreferenceManager getInstance() {
+        if (mInstance == null) {
+
+            mInstance = new PreferenceManager();
+        }
+
+        return mInstance;
+    }
+
+    public void put(String key, String value) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public void put(String key, Boolean value) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+}
